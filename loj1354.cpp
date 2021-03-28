6b105d33d638ae49129991698a31aa69e027cd1e
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2021-03-28 11:56:15 +0600

    Add files via upload

---
+++ loj1354.cpp
@@ -0,0 +1,166 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nn int
+#define dd double
+#define ll long long int
+#define ss string
+
+
+//reguler Bracket sequence function
+/*bool RBC(string s)
+{
+    stack<char>st;
+    char x;
+
+    for(nn i=0;i<s.size();i++)
+    {
+        if(s[i]=='(' || s[i]=='{' || s[i]=='[') 
+            {
+                st.push(s[i]);
+                continue;
+            }
+        if(st.empty()) return false;
+
+        if(s[i]==')')
+        {
+            x=st.top();
+            st.pop();
+            if(x=='{' || x=='[') return false;
+        }
+        if(s[i]=='}')
+        {
+            x=st.top();
+            st.pop();
+            if(x=='(' || x=='[') return false;
+        }
+        if(s[i]==']')
+        {
+            x=st.top();
+            st.pop();
+            if(x=='(' || x=='{') return false;
+        }
+    }
+    return (st.empty());
+
+
+}*/
+
+
+
+//prime_seive_algorithm
+/*ll p[1000005]={0};
+void prime_seive()
+{
+       for(ll i=3;i<1000005;i+=2) p[i]=1;
+
+       for(ll i=3;i<1000005;i+=2)
+       {
+           if(p[i]==1)
+           {
+               for(ll j=i*i;j<1000005;j+=i) p[j]=0;
+           }
+       }
+       p[2]=1;
+       p[1]=p[0]=0;
+
+}*/
+
+
+
+//pelindrom number
+/*bool pelindrom_number(ll x)
+{
+  ll tt=x,t,ttt=0;
+  while(tt!=0)
+  {
+    t=tt%10;
+    ttt=ttt*10+t;
+    tt/=10;
+  }
+  if(x==ttt) return true;
+  else return false;
+
+}*/
+
+//Bigmod algorithm
+/*ll BigMod(ll a,ll b,ll p)
+{ 
+     ll ans = 1;
+     a%=p;
+     if(a==0) return 0;
+   
+       while(b>0)
+          {
+              if(b & 1)  ans=(ans*a)%p;
+
+               b/=2;
+
+               a=(a*a)%p;
+  
+          }
+
+  return ans;
+
+}*/
+int binary_to_decimal(ll a)
+{
+  nn number = 0,i=0,rem;
+  while(a!=0)
+  {
+    rem = a%10;
+    a/=10;
+
+    number+=rem*pow(2,i);
+    i++;
+  }
+  return number;
+}
+nn j=0;
+void code()
+{
+      ss s,y,x;
+      cin>>s>>y;
+      vector<nn>v;
+      vector<nn>v2;
+      for(nn i=0;i<s.size();i++)
+      {
+        if(s[i]!='.') x+=s[i];
+        else v.push_back(stoi(x)),x.clear();
+          
+
+      }
+      v.push_back(stoi(x)),x.clear();
+
+      for(nn i=0;i<y.size();i++)
+      {
+        if(y[i]!='.') x+=y[i];
+        else v2.push_back(stoi(x)),x.clear();
+          
+
+      }
+      v2.push_back(stoi(x)),x.clear();
+      nn flag=1;
+      for(nn i=0;i<v.size();i++)
+      {
+        if(v[i]!= binary_to_decimal(v2[i]))
+        {
+          flag=0;
+          break;
+        }
+      }
+      ++j;
+      cout<<"Case "<<j<<": ";
+      if(flag) cout<<"Yes"<<"\n";
+      else cout<<"No"<<"\n";
+ 
+
+ }
+      
+int main()
+{
+  nn tt;
+  cin>>tt;
+  while(tt--) code();
+
+    
+}
